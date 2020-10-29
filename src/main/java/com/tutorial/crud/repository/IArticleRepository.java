f1f8c316a24ee5ce86e6bb3c82021bae1b1e0fe0
Pablo Giraldo <pablogirald@yahoo.es>
2020-10-29 12:56:25 +0100

    Add singleton

--- src/main/java/com/tutorial/crud/repository/IArticleRepository.java
+++ src/main/java/com/tutorial/crud/repository/IArticleRepository.java
@@ -3,6 +3,8 @@ package com.tutorial.crud.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import com.tutorial.crud.entity.Article;
 
 @Repository
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public interface IArticleRepository extends JpaRepository<Article, Long> {
 	public List<Article> findAllByOrderByIdDesc();
 
--- src/main/java/com/tutorial/crud/repository/ICategoryRepository.java
+++ src/main/java/com/tutorial/crud/repository/ICategoryRepository.java
@@ -2,17 +2,18 @@ package com.tutorial.crud.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tutorial.crud.entity.Category;
 
 @Repository
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
 
 //	List<Category> findAllByOrderByIdDesc();
-	
+
 	List<Category> findAllByOrderByNameAsc();
 
 	Category findByName(String name);
